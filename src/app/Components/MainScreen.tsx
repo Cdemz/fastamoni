b6ecf452bd3fc9f0bda0f57973db64aaa63a332a
Ademola <gbengaadepoju75@gmail.com>
2024-07-02 06:31:21 -0500

    animation

--- src/app/Components/MainScreen.tsx
+++ src/app/Components/MainScreen.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const MainScreen = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1200, // Set the duration of the animation
+      once: false, // Whether animation should happen only once - while scrolling down
+    });
+  }, []);
   return (
-    <div className="jost bg-black h-screen text-white  mt-14 p-8 lg:pl-[8rem] flex flex-col lg:gap-8 gap-4 ">
+    <div
+      className="jost bg-black h-screen text-white  mt-14 p-8 lg:pl-[8rem] flex flex-col lg:gap-8 gap-4 "
+      data-aos="fade-left"
+    >
       <h3 className=" font-semibold mt-16">WELCOME TO CODESHAPER</h3>
       <h1 className="font-bold text-5xl lg:text-6xl">
         <span className="block mb-4 lg:mb-6">CONSULTING</span>
